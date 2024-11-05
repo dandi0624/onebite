a87c04d4eaad4c2a32b1dd7a497fcf98aa7558dc
dandi0624 <153481807+dandi0624@users.noreply.github.com>
2024-11-05 16:58:15 +0900

    add : TodoList
    - TodoList CSS

---
+++ section08/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import Header from './components/Header';
+import Editor from './components/Editor';
+import List from './components/List';
+import {useState} from 'react';
+
+function App() {
+
+
+  return (
+    <div className="App">
+    <section>
+      <Header />
+    </section>
+    <section>
+      <Editor />
+    </section>
+    <section>
+      <List />
+    </section>
+    </div>
+  )
+}
+
+export default App
---
+++ section08/src/components/List.jsx
@@ -0,0 +1,16 @@
+import './List.css';
+import TodoItem from './Todoitem';
+
+const List = () =>{
+    return <div className="List">
+        <h4>Todo List 📌</h4>
+        <input placeholder='검색어를 입력하세요'/>
+        <div className="todos_wapper">
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+        </div>
+    </div>
+}
+
+export default List;
