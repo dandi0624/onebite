c0c2c1188756d94f4b8ae140340ff925fbe94503
dandi0624 <153481807+dandi0624@users.noreply.github.com>
2024-11-07 13:05:53 +0900

    modify

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -4,8 +4,31 @@ import Editor from './components/Editor';
 import List from './components/List';
 import {useState} from 'react';
 
+const mokData=[
+  {
+    id:0,
+    isDone : false,
+    content : "React 공부하기",
+    date : new Date().getTime(),
+  },
+  {
+    id:1,
+    isDone : false,
+    content : "빨래하기",
+    date : new Date().getTime(),
+  },
+  {
+    id:2,
+    isDone : false,
+    content : "노래 연습하기",
+    date : new Date().getTime(),
+  },
+]
+
 function App() {
 
+  
+  const [todos, setTodos] = useState([mokData]);
 
   return (
     <div className="App">
