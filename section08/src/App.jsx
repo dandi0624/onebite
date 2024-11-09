a20bc71d233cf0e4318deecf2580026e94153eb1
dandi0624 <153481807+dandi0624@users.noreply.github.com>
2024-11-09 12:19:44 +0900

    add : create 기능
    - 투두 추가하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,48 +1,59 @@
-import './App.css'
-import Header from './components/Header';
-import Editor from './components/Editor';
-import List from './components/List';
-import {useState} from 'react';
+import "./App.css";
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
+import { useState, useRef } from "react";
 
-const mokData=[
+const mokData = [
   {
-    id:0,
-    isDone : false,
-    content : "React 공부하기",
-    date : new Date().getTime(),
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
   },
   {
-    id:1,
-    isDone : false,
-    content : "빨래하기",
-    date : new Date().getTime(),
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
   },
   {
-    id:2,
-    isDone : false,
-    content : "노래 연습하기",
-    date : new Date().getTime(),
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
   },
-]
+];
 
 function App() {
-
-  
   const [todos, setTodos] = useState([mokData]);
+  const idRef = useRef(3);
+
+  // 새로운 일정 추가하기
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
 
   return (
     <div className="App">
-    <section>
-      <Header />
-    </section>
-    <section>
-      <Editor />
-    </section>
-    <section>
-      <List />
-    </section>
+      <section>
+        <Header />
+      </section>
+      <section>
+        <Editor onCreate={onCreate} />
+      </section>
+      <section>
+        <List />
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
