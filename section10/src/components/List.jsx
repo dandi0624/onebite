b8fa21b5bf5964c91b7e1f3f31d00ae034a3ef09
dandi0624 <153481807+dandi0624@users.noreply.github.com>
2024-11-14 13:45:52 +0900

    add : 최적화
    - useCallback과 함수 재생성 방지

---
+++ section10/src/components/List.jsx
@@ -0,0 +1,82 @@
+import "./List.css";
+import TodoItem from "./Todoitem";
+import { useState, useMemo } from "react";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
+  // const getAnalyzedData = () => {
+  //   console.log("getAnalyzedData 호출")
+  //   const totalCount = todos.length;
+  //   const doneCount = todos.filter((todo)=>todo.isDone).length;
+  //   const notDoneCount = totalCount - doneCount;
+
+  //   return {
+  //     totalCount,
+  //     doneCount,
+  //     notDoneCount
+  //   }
+  // } 이 함수를 아래 useMemo로 활용
+
+  const {totalCount, doneCount, notDoneCount} = useMemo(()=>{
+    console.log("getAnalyzedData 호출")
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo)=>todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount
+    }
+  }, [todos])
+  // 의존성 배열 : deps
+
+  // const {totalCount, doneCount, notDoneCount} = getAnalyzedData()
+
+  return (
+    <div className="List">
+      <h4>Todo List 📌</h4>
+      <div>
+        <div>total : {totalCount}</div>
+        <div>done : {doneCount}</div>
+        <div>notDoneCount : {notDoneCount}</div>
+      </div>
+
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
