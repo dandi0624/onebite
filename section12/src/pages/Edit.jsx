bd514fcaf12453d11a2607bb614a5c54b62f3b39
dandi0624 <153481807+dandi0624@users.noreply.github.com>
2024-12-04 16:51:54 +0900

    add : Diary 기능 구현

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,8 +1,45 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Editor from '../components/Editor';
+import { useContext, useEffect, useState } from "react";
+import { DiaryDispatchContext, DiaryStateContext } from "../App";
+import useDiary from "../hooks/useDiary";
+import usePageTitle from "../hooks/usePageTitle";
 
 const Edit = () =>{
     const params = useParams();
-    return <div>{params.id}번 일기 수정페이지입니다.</div>
+    const nav = useNavigate();
+    const {onDelete, onUpdate} = useContext(DiaryDispatchContext);
+    usePageTitle(`${params.id}번 일기 수정`);
+
+    const curDiaryItem = useDiary(params.id);
+
+    const onClickDelete = () =>{
+        if(
+        window.confirm("일기를 정말 삭제하시겠습니까? 다시 복구되지 않습니다.")
+        ) {
+            // 일기 삭제 로직
+            onDelete(params.id);
+            nav('/',{replace:true});
+        }
+    }    
+
+
+    const onSubmit = (input)=>{
+        if(window.confirm("일기를 정말 수정할까요?")){
+        onUpdate(params.id, input.createdDate.getTime(), input.emotionId, input.content);
+        }
+        nav('/',{replace:true})
+    }
+
+    return <div>
+        <Header title={"일기 수정하기"}
+        leftChild={<Button onClick={()=>nav(-1)} text={"< 뒤로가기"}/>}
+        rightChild={<Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"}/>}
+        />
+        <Editor initData={curDiaryItem}/>
+    </div>
 }
 
 export default Edit;
